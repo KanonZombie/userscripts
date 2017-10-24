318d76a64a7aec0fe01c73248d62bba6da393318
KanonZombie <33068818+KanonZombie@users.noreply.github.com>
2017-10-24 15:11:29 -0300

    Update krikzz_pesos.user.js

--- krikzz_pesos.user.js
+++ krikzz_pesos.user.js
@@ -3,33 +3,12 @@
 // @namespace   http://zombie
 // @description krikzz pesos
 // @include     https://krikzz.com/store/
+// @require     https://raw.githubusercontent.com/KanonZombie/userscripts/master/funciones.js
 // @version     1
 // @grant       none
 // ==/UserScript==
 
-var fechaConsulta = new Date( localStorage.getItem('fechaConsulta') );
-var today = new Date();
-
-fechaConsulta.setHours(0,0,0,0);
-today.setHours(0,0,0,0);
-
-if ( fechaConsulta == null || fechaConsulta <  today )
-{
-  console.log( "guardando" );
-
-  $.ajax({
-      url: "https://openexchangerates.org/api/latest.json?app_id=7a53d03067514dce8d68ccfbacf276c4",
-      async:false
-    }).done(function( data ) {
-      alert( "Actualizando cotizacion dolar" );
-        //cotizDolar =  ;
-        localStorage.setItem('fechaConsulta', today);
-        localStorage.setItem('cotizDolar', parseFloat( data["rates"]["ARS"] ));
-      });
-}
-
-var cotizDolar = parseFloat( localStorage.getItem('cotizDolar') );
-
+var cotizDolar = ObtenerCotizacionDolar();
 
 $('.right-block .content_price .product-price').each(function( index ) {
   console.log( index + ": " + parseFloat( $( this ).text().replace(/\$/, "").replace(/,/g,'') ));
@@ -37,10 +16,3 @@ $('.right-block .content_price .product-price').each(function( index ) {
   var pesos = precio * cotizDolar * 1.5;
   $(this).append( '('+FormatearImporte(pesos)+')' );
 });
-
-
-function FormatearImporte( cantidad )
-{
-console.log(cantidad);
-  return cantidad.toFixed(2);
-}
