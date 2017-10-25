274890dd7530b71dbda0b171b52d2c1776fe8226
KanonZombie <33068818+KanonZombie@users.noreply.github.com>
2017-10-25 09:07:05 -0300

    Update krikzz_pesos.user.js

--- krikzz_pesos.user.js
+++ krikzz_pesos.user.js
@@ -3,6 +3,7 @@
 // @namespace   http://zombie
 // @description krikzz pesos
 // @include     https://krikzz.com/store/
+// @include     https://krikzz.com/store/order
 // @require     https://raw.githubusercontent.com/KanonZombie/userscripts/master/funciones.js
 // @version     1
 // @grant       none
@@ -10,9 +11,23 @@
 
 var cotizDolar = ObtenerCotizacionDolar();
 
-$('.right-block .content_price .product-price').each(function( index ) {
-  console.log( index + ": " + parseFloat( $( this ).text().replace(/\$/, "").replace(/,/g,'') ));
-  var precio = parseFloat( $( this ).text().replace(/\$/, "").replace(/,/g,'') );
-  var pesos = precio * cotizDolar * 1.5;
-  $(this).append( '('+FormatearImporte(pesos)+')' );
-});
+alert( window.location.href.indexOf('https://krikzz.com/store/')>=0 );
+
+if ( window.location.href.indexOf('https://krikzz.com/store/')>=0 )
+{
+  $('.right-block .content_price .product-price').each(function( index ) {
+    var precio = parseFloat( $( this ).text().replace(/\$/, "").replace(/,/g,'') );
+    var pesos = precio * cotizDolar * 1.5;
+    $(this).append( '('+FormatearImporte(pesos)+')' );
+  });
+}
+
+if ( window.location.href.indexOf('https://krikzz.com/store/order')>=0 )
+{
+  $('#order-detail-content tr td.price').each(function( index ) {
+    var precio = parseFloat( $( this ).text().replace(/\$/, "").replace(/,/g,'') );
+    var pesos = precio * cotizDolar * 1.5;
+    $(this).append( '('+FormatearImporte(pesos)+')' );
+  });
+}
+
