afb8cb12b33008058e3b6d9ecd4d8f4fcb62fc0d
KanonZombie <33068818+KanonZombie@users.noreply.github.com>
2019-02-28 23:09:39 -0300

    Create rtwanttosee.user.js

---
+++ rtwanttosee.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        RottenTomatoes Bring back want to see
+// @namespace   http://zombie
+// @include     https://www.rottentomatoes.com/m/*
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+
+$('#rating_widget_modal').css('display', 'block')
