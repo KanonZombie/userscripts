a235bd610df7ffff28fb7d716415f40fa0d8a194
KanonZombie <33068818+KanonZombie@users.noreply.github.com>
2023-06-30 17:13:53 -0300

    Formato

--- MegaBuild.user.js
+++ MegaBuild.user.js
@@ -11,191 +11,168 @@
 const _nombreScript = 'Mega Build';
 const _linkScript = 'https://github.com/KanonZombie/userscripts/raw/master/MegaBuild.user.js';
 
-Notification.requestPermission().then(function(result) {
-	  console.log('Estado permisos de notificaciones: ',result);
+Notification.requestPermission().then(function (result) {
+    console.log('Estado permisos de notificaciones: ', result);
 });
 
 CheckCambiosProcesos();
-setInterval( function() { CheckCambiosProcesos() }, 60000 ) ;
+setInterval(function () { CheckCambiosProcesos() }, 60000);
 
-if (  window.location.href.indexOf( "/AutobuildEstado/VerAutobuild/" ) > 0 )
-{
-  var nroCorrida = window.location.href.match(/\/AutobuildEstado\/VerAutobuild\/([0-9]*)/)[1];
+if (window.location.href.indexOf("/AutobuildEstado/VerAutobuild/") > 0) {
+    var nroCorrida = window.location.href.match(/\/AutobuildEstado\/VerAutobuild\/([0-9]*)/)[1];
 
-  $('.contGral > h4:nth-child(6)').append( '<div><button id="Memorizar" class="row btn colorcorrida text-white bancodeprueba" onclick="localStorage.setItem(\'MegaBuild_nroCorrida\', ' + nroCorrida + ');">Memorizar</button></div>' );
+    $('.contGral > h4:nth-child(6)').append('<div><button id="Memorizar" class="row btn colorcorrida text-white bancodeprueba" onclick="localStorage.setItem(\'MegaBuild_nroCorrida\', ' + nroCorrida + ');">Memorizar</button></div>');
 
-  $(document).ready(function()
-                    {
-    									setTimeout(function()
-                                 {
-                        							$('#MainTBody > tr > td.enproceso').each( function(i, td)
-                                                                               {
-																																							   //console.log( td );
- 																					                                       //td.click();
-																																							 } );
-                      					 }
-                                 , 1000
-                                 )
-  									}
-                    );
-}
-
-if (  window.location.href.indexOf( "Home/Index" ) > 0 )
-{
-  var nroCorrida = localStorage.getItem('MegaBuild_nroCorrida');
-
-  if ( nroCorrida )
-  {
-	  $('li.ic2:nth-child(2) > a:nth-child(1)').attr("href", "/AutobuildEstado/VerAutobuild/" + nroCorrida);
-  }
-
-  $('li.ic2:nth-child(4) > a:nth-child(1)').attr("href", "/Maquina/Index");
-}
-
-if (  window.location.href.indexOf( "AutobuildEstado/ActivosBancoPrueba" ) > 0 )
-{
-  ColorearFilas( "tr.row" );
-
-  var grillaOld = $('#grilla').clone();
-  var navOld = $('#cuerpo > nav:nth-child(2)').clone();
-  var tituloOld = $('.subtitulo').clone();
-
-  $('#cuerpo > nav:nth-child(2)').remove();
-
-  var userData = $('.user-data')[0].textContent;
-
-  var usuarioLogueado = /Bienvenido, (.+)/g.exec(userData)[1];
-
-  $('.subtitulo h2')[0].textContent = 'Bancos de ' + usuarioLogueado;
-
-  var filas = $("tr.row");
-
-  for ( i=1; i<filas.length; i++ )
-  {
-    var usuario = filas[i].cells[1].textContent.trim();
-
-    if ( usuario != usuarioLogueado )
-    {
-      filas[i].remove();
+    $(document).ready(function () {
+        setTimeout(function () {
+            $('#MainTBody > tr > td.enproceso').each(function (i, td) {
+                //console.log( td );
+                //td.click();
+            });
+        }
+            , 1000
+        )
     }
-  }
-
-  $("#cuerpo").append( tituloOld );
-  $("#cuerpo").append( navOld );
-  $("#cuerpo").append( grillaOld );
+    );
 }
 
-if (  window.location.href.indexOf( "Maquina/Index" ) > 0 )
-{
-	var filas = $('.tabla-detalles > tbody:nth-child(1) > tr')
+if (window.location.href.indexOf("Home/Index") > 0) {
+    var nroCorrida = localStorage.getItem('MegaBuild_nroCorrida');
 
-  for ( i=1; i<filas.length; i++ )
-  {
-    var proceso = filas[i].cells[6].textContent.trim();
-    if ( proceso !== '' )
-    {
-      let contenido = filas[i].cells[6].children[1].innerHTML ;
-      var idProceso = contenido.match(/\(([0-9]*)\)/)[1];
-
-      var match = contenido.replace(/\(([0-9]*)\)/g, "(<a href='/AutobuildEstado/VerAutobuild/"+idProceso+"'>" + idProceso + "</a>)");
-
-      filas[i].cells[6].children[1].innerHTML = match;
+    if (nroCorrida) {
+        $('li.ic2:nth-child(2) > a:nth-child(1)').attr("href", "/AutobuildEstado/VerAutobuild/" + nroCorrida);
     }
-  }
 
-  setInterval( function() { location.reload() }, 30000 ) ;
+    $('li.ic2:nth-child(4) > a:nth-child(1)').attr("href", "/Maquina/Index");
 }
 
-function spawnNotification(theBody,theIcon,theTitle) {
+if (window.location.href.indexOf("AutobuildEstado/ActivosBancoPrueba") > 0) {
+    ColorearFilas("tr.row");
 
-  if (!theTitle)
-  {
-    theTitle = _nombreScript;
-  }
+    var grillaOld = $('#grilla').clone();
+    var navOld = $('#cuerpo > nav:nth-child(2)').clone();
+    var tituloOld = $('.subtitulo').clone();
 
-  if (!theIcon)
-  {
-    theIcon = "https://images.vexels.com/media/users/3/199841/isolated/lists/96a7cac08ad4539e1888d8f5c82b5f48-icono-de-coronavirus-covid19.png";
-  }
+    $('#cuerpo > nav:nth-child(2)').remove();
 
-	if (Notification.permission === "granted") {
-			var options = {
-      	body: theBody,
-      	icon: theIcon
-  		}
-  		var n = new Notification(theTitle,options);
+    var userData = $('.user-data')[0].textContent;
 
-    	//https://www.thesoundarchive.com/starwars/WilhelmScream.mp3
-      var audio = new Audio("https://www.thesoundarchive.com/ringtones/pacman_death.mp3");
-      audio.play();
-  }
+    var usuarioLogueado = /Bienvenido, (.+)/g.exec(userData)[1];
+
+    $('.subtitulo h2')[0].textContent = 'Bancos de ' + usuarioLogueado;
+
+    var filas = $("tr.row");
+
+    for (i = 1; i < filas.length; i++) {
+        var usuario = filas[i].cells[1].textContent.trim();
+
+        if (usuario != usuarioLogueado) {
+            filas[i].remove();
+        }
+    }
+
+    $("#cuerpo").append(tituloOld);
+    $("#cuerpo").append(navOld);
+    $("#cuerpo").append(grillaOld);
 }
 
-function htmlDecode(input){
-  var e = document.createElement('textarea');
-  e.innerHTML = input;
-  // handle case of empty input
-  return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+if (window.location.href.indexOf("Maquina/Index") > 0) {
+    var filas = $('.tabla-detalles > tbody:nth-child(1) > tr')
+
+    for (i = 1; i < filas.length; i++) {
+        var proceso = filas[i].cells[6].textContent.trim();
+        if (proceso !== '') {
+            let contenido = filas[i].cells[6].children[1].innerHTML;
+            var idProceso = contenido.match(/\(([0-9]*)\)/)[1];
+
+            var match = contenido.replace(/\(([0-9]*)\)/g, "(<a href='/AutobuildEstado/VerAutobuild/" + idProceso + "'>" + idProceso + "</a>)");
+
+            filas[i].cells[6].children[1].innerHTML = match;
+        }
+    }
+
+    setInterval(function () { location.reload() }, 30000);
 }
 
-function CheckCambiosProcesos()
-{
-     $.ajax({
+function spawnNotification(theBody, theIcon, theTitle) {
+
+    if (!theTitle) {
+        theTitle = _nombreScript;
+    }
+
+    if (!theIcon) {
+        theIcon = "https://images.vexels.com/media/users/3/199841/isolated/lists/96a7cac08ad4539e1888d8f5c82b5f48-icono-de-coronavirus-covid19.png";
+    }
+
+    if (Notification.permission === "granted") {
+        var options = {
+            body: theBody,
+            icon: theIcon
+        }
+        var n = new Notification(theTitle, options);
+
+        //https://www.thesoundarchive.com/starwars/WilhelmScream.mp3
+        var audio = new Audio("https://www.thesoundarchive.com/ringtones/pacman_death.mp3");
+        audio.play();
+    }
+}
+
+function htmlDecode(input) {
+    var e = document.createElement('textarea');
+    e.innerHTML = input;
+    // handle case of empty input
+    return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+}
+
+function CheckCambiosProcesos() {
+    $.ajax({
         url: 'https://newautobuild.zoologicnet.com.ar/Maquina/Index',
         method: 'GET',
         success: function (data) {
-          var datoTabla = data.match(/<table class="tabla-detalles table table-striped table-hover">[\s\S]*<\/table>/)[0];
+            var datoTabla = data.match(/<table class="tabla-detalles table table-striped table-hover">[\s\S]*<\/table>/)[0];
 
-            var filas = datoTabla.match( /<tr>([\s\S]*?)<\/tr>/g );
+            var filas = datoTabla.match(/<tr>([\s\S]*?)<\/tr>/g);
 
             let MegaBuild_Procesos = new Map();
 
-            for ( i=1; i<filas.length; i++ )
-            {
+            for (i = 1; i < filas.length; i++) {
                 var celdas = filas[i].match(/<td>[\s\S]*?<\/td>/g);
-                var nombrePC = celdas[1].match( /<td>([\s\S]*?)<\/td>/ )[1].trim().toUpperCase();
-                var contenidoProcesando = htmlDecode ( celdas[6].match( /<td>([\s\S]*?)<\/td>/ )[1].trim() );
+                var nombrePC = celdas[1].match(/<td>([\s\S]*?)<\/td>/)[1].trim().toUpperCase();
+                var contenidoProcesando = htmlDecode(celdas[6].match(/<td>([\s\S]*?)<\/td>/)[1].trim());
 
-                var procesando = contenidoProcesando.match(/<p[\s\S]*?>([\s\S]*?)\(([0-9]*)\)<\/p>/ );
-                if ( procesando )
-                {
+                var procesando = contenidoProcesando.match(/<p[\s\S]*?>([\s\S]*?)\(([0-9]*)\)<\/p>/);
+                if (procesando) {
 
-                var usuarioDelBP;
-                $.ajax({
-                    url: 'https://newautobuild.zoologicnet.com.ar/AutobuildEstado/VerAutobuild/'+procesando[2],
-                    method: 'GET',
-                    async: false,
-                    success: function (dataBP) {
-                        usuarioDelBP = dataBP.match(/<th class="colorcorrida" scope="row">Usuario:<\/th>\W+?<td>(.+?)<\/td>/)[1];
-                    }
-                });
-                var detalleProceso = procesando[1] + " (" + procesando[2] + ") " + usuarioDelBP.trim().toUpperCase() ;
+                    var usuarioDelBP;
+                    $.ajax({
+                        url: 'https://newautobuild.zoologicnet.com.ar/AutobuildEstado/VerAutobuild/' + procesando[2],
+                        method: 'GET',
+                        async: false,
+                        success: function (dataBP) {
+                            usuarioDelBP = dataBP.match(/<th class="colorcorrida" scope="row">Usuario:<\/th>\W+?<td>(.+?)<\/td>/)[1];
+                        }
+                    });
+                    var detalleProceso = procesando[1] + " (" + procesando[2] + ") " + usuarioDelBP.trim().toUpperCase();
 
-                MegaBuild_Procesos.set( nombrePC, detalleProceso );
+                    MegaBuild_Procesos.set(nombrePC, detalleProceso);
 
                 }
-          }
+            }
 
-            var oldProcesos = new Map(JSON.parse( localStorage.getItem('MegaBuild_Procesos') ));
+            var oldProcesos = new Map(JSON.parse(localStorage.getItem('MegaBuild_Procesos')));
 
-            if ( oldProcesos )
-            {
+            if (oldProcesos) {
 
-                for (let [key, value] of MegaBuild_Procesos)
-                {
-                    var estadoAnterior = oldProcesos.get( key );
-                    if ( !estadoAnterior )
-                    {
+                for (let [key, value] of MegaBuild_Procesos) {
+                    var estadoAnterior = oldProcesos.get(key);
+                    if (!estadoAnterior) {
                         spawnNotification(key + ' empezó a ejecutar ' + value);
                     }
-                    else
-                    {
-                        if( estadoAnterior != value)
-                        {
+                    else {
+                        if (estadoAnterior != value) {
                             spawnNotification(key + ' ahora esta ejecutando ' + value);
                         }
-                        else
-                        {
+                        else {
                             //console.log(key + ' esta haciendo lo mismo:' + value + " y " + estadoAnterior);
                         }
                     }
@@ -203,27 +180,23 @@ function CheckCambiosProcesos()
             }
 
 
-            localStorage.setItem('MegaBuild_Procesos', JSON.stringify(Array.from(MegaBuild_Procesos.entries())) );
+            localStorage.setItem('MegaBuild_Procesos', JSON.stringify(Array.from(MegaBuild_Procesos.entries())));
 
         } // fin callback
-  	});
+    });
 }
 
-function ColorearFilas( selectorCSS )
-{
-  var filas = $( selectorCSS );
-  for ( i=1; i<filas.length; i++ )
-  {
-    //style="background-color: #7EF584"
-    var usuario2 = filas[i].cells[6].firstElementChild.src;
-    if ( usuario2.indexOf( 'error.png' ) > 0 )
-    {
-      $(filas[i]).css("background-color", "#FFa6a6");
-    }
+function ColorearFilas(selectorCSS) {
+    var filas = $(selectorCSS);
+    for (i = 1; i < filas.length; i++) {
+        //style="background-color: #7EF584"
+        var usuario2 = filas[i].cells[6].firstElementChild.src;
+        if (usuario2.indexOf('error.png') > 0) {
+            $(filas[i]).css("background-color", "#FFa6a6");
+        }
 
-    if ( usuario2.indexOf( 'ok.png' ) > 0 )
-    {
-      $(filas[i]).css("background-color", "#A6FFA6");
+        if (usuario2.indexOf('ok.png') > 0) {
+            $(filas[i]).css("background-color", "#A6FFA6");
+        }
     }
-  }
 }
